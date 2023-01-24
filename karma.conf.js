663a588a8b65f233848f3c53e279af90e6c9d884
Lenin V <lcvelastegui@gmail.com>
2023-01-23 23:27:21 -0500

    test 2 init proyecto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/test-big-front'),
+      dir: require('path').join(__dirname, './coverage/proyect'),
       subdir: '.',
       reporters: [
         { type: 'html' },
